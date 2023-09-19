64f55b41ba2eaabefe4ce39771dc39ee6cc4aa4c
M2i_Beffroi <mon.email@exemple.fr>
2023-09-19 09:58:33 +0200

    ContiExo

--- Exo_Spring/Exo1/src/main/java/com/example/exo1/controllers/BaseController.java
+++ Exo_Spring/Exo1/src/main/java/com/example/exo1/controllers/BaseController.java
@@ -5,57 +5,24 @@ import com.example.exo1.models.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
-@RequestMapping("/")
+@RequestMapping("/base") //http://localhost:8080/ renvoi avec une page html (vue)
 @Controller
 public class BaseController {
-    @GetMapping("/personDetail")
-        public String personDetails(Model model) {
+    @GetMapping("/home")
+        public String home(Model model) {
             Person person = new Person("Leetchi","Leet", "leetchileet@test.com");
             model.addAttribute("person", person);
-            return "PersonDetail";
+            return "home";
         }
 
-        @GetMapping("/api/v1/person-details")
-        @ResponseBody
-        public Person apiPersonDetails() {
-            return new Person("Yugi", "giho", "yugiho@test.com");
-        }
 
-        @GetMapping("/text-data")
+        @GetMapping("/textData")
         public String textData(Model model) {
-            model.addAttribute("text", "Ceci est un élément simple de type textuel.");
+            model.addAttribute("text", "blablabla");
             return "text-data";
         }
 
-        @GetMapping("/api/v1/text-data")
-        @ResponseBody
-        public String apiTextData() {
-            return "Ceci est un élément simple de type textuel.";
-        }
-
-        @GetMapping("/text-list")
-        public String textList(Model model) {
-            List<String> texts = new ArrayList<>();
-            texts.add("Texte 1");
-            texts.add("Texte 2");
-            model.addAttribute("texts", texts);
-            return "text-list";
-        }
-
-        @GetMapping("/api/v1/text-list")
-        @ResponseBody
-        public List<String> apiTextList() {
-            List<String> texts = new ArrayList<>();
-            texts.add("Texte 1");
-            texts.add("Texte 2");
-            return texts;
-        }
 }
 
 
---
+++ Exo_Spring/Exo1/src/main/java/com/example/exo1/controllers/BaseRestController.java
@@ -0,0 +1,44 @@
+package com.example.exo1.controllers;
+
+import ch.qos.logback.core.model.Model;
+import com.example.exo1.models.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseRestController {
+
+ // Pour l'api on met @responseBody et renvoi en json
+
+    @GetMapping("/api/v1/detail") //http://localhost:8080/api/v1/base
+    @ResponseBody
+    public Person detail() {
+        return new Person("Yugi", "giho", "yugiho@test.com");
+    }
+
+    @GetMapping("/api/v1/TextData")
+    @ResponseBody
+    public String apiTextData() {
+        return "bliblibli";
+    }
+
+    @GetMapping("/textlist")
+    public String textList(Model model) {
+        List<String> texts = new ArrayList<>();
+        texts.add("Texte 1");
+        texts.add("Texte 2");
+        model.addAttribute("texts", texts);
+        return "text-list";
+    }
+
+    @GetMapping("/api/v1/TextList")
+    @ResponseBody
+    public List<String> apiTextList() {
+        List<String> texts = new ArrayList<>();
+        texts.add("Texte 1");
+        texts.add("Texte 2");
+        return texts;
+    }
+}
